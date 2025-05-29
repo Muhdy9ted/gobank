9fef09edea0f4dc5637b5c716b1d3900a1f7bf67
Muhammed Abdullahi <muhammed.abdullahi@swisscom.com>
2025-05-29 21:23:26 +0200

    db crud unit test

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+)
+
+const (
+	dbSource = "postgresql://admin:admin@123@localhost:5432/go-bank-postgres?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := pgx.Connect(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
---
+++ util/test-helper/random.go
@@ -0,0 +1,44 @@
+package test_helper
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	if min >= max {
+		return min
+	}
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() float64 {
+	return float64(RandomInt(100, 1000000))
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR"}
+	return currencies[rand.Intn(len(currencies))]
+}
